33f1d01ab7932d10d5beaecd5503a35604f6bb6c
Alerrando Breno <alerrando2@gmail.com>
2022-04-27 19:49:08 -0300

    Projeto Relógio Digital

---
+++ script.js
@@ -0,0 +1,60 @@
+window.onload = function(){
+    data = new Date();
+    var horas=00, minutos=00, segundos=00;
+    var $horas = document.getElementById("hora");
+    var $minutos = document.getElementById("minutos");
+    var $segundos = document.getElementById("segundos");
+    var $AMPM = document.getElementById("AMPM");
+
+    horas = data.getHours();
+    minutos = data.getMinutes();
+    segundos = data.getSeconds();
+
+    if(horas >= 12)
+        $AMPM.innerHTML = "PM";
+    else
+        $AMPM.innerHTML = "AM";
+
+    $horas.innerHTML = horas;
+    $minutos.innerHTML = minutos;
+    $segundos.innerHTML = segundos;
+
+    function startTimer(){
+        segundos++;
+
+        if(horas == 0)
+        {
+            horas = (horas - 12) + "";
+            $horas.innerHTML = horas;
+        }
+
+        if(minutos > 60)
+        {
+            minutos = "00";
+            $minutos.innerHTML = minutos;
+            horas++;
+            $horas.innerHTML = "0" + horas;
+        }
+
+        if(segundos <= 9)
+            $segundos.innerHTML = "0" + segundos;
+
+        if(segundos > 9)
+            $segundos.innerHTML = segundos;
+
+        if(segundos > 99)
+        {
+            segundos = "00";
+            minutos++;
+            if(minutos <= 9)
+                $minutos.innerHTML = "0" + minutos;
+            else
+                $minutos.innerHTML = minutos;
+
+            $segundos.innerHTML = segundos;
+        }
+
+    }
+
+    setInterval(startTimer, 1000);
+}
